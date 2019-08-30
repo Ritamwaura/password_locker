aa409c139962d9b58c3349d9360c138168e4f81e
Your Name <mwaurarita2019@gmail.com>
2019-08-30 14:59:06 +0300

    identation fixed

--- user.py
+++ user.py
@@ -1,33 +1,31 @@
-class user:
+class User:
     
-    user_list = []
-    
-    
-   def __init__(self,first_name,last_name,name,password):
-     
+    user_list = []  
+
+    def __init__(self,first_name,last_name,name,password):
+ 
 
 
         self.first_name = first_name
         self.last_name = last_name
         self.username = name
         self.password = password
+
         
+
     def save_user(self):
-    
+
         '''
         save_contact method saves contact objects into contact_list
         '''
 
         User.user_list.append(self)  
-        
 
 
     def delete_user(self):
-    
+
         User.user_list.remove(self)
-        
-        
-        
+
     @classmethod
     def find_by_name(cls,name):
 
@@ -36,31 +34,27 @@ class user:
             if user.username == name:
                 return user
             
-            
-    
     @classmethod
     def user_exist(cls,name):
 
         for user in cls.user_list:
             if user.username == name:
                     return True
-                
-                
+
+        return False
+    
     @classmethod
     def display_users(cls):
 
         return cls.user_list
 
-        return False
-    
-    
     @classmethod
     def copy_password(cls,name):
         user_found = User.find_by_name(name)
         pyperclip.copy(user_found.password)
         
-        
-       @classmethod
+
+    @classmethod
     def display_user(cls,password):
 
         user_user_list = []
@@ -70,8 +64,8 @@ class user:
                 user_user_list.append(password)
 
         return user_user_list
-    
-        @classmethod
+
+    @classmethod
     def log_in(cls, name, password):
 
     
@@ -80,5 +74,6 @@ class user:
                 return Credential.credential_list
 
         return False
+
         
     
