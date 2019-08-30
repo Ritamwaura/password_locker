ae1c7b5fe21a5ed12449657cd29b4c1d4704661b
Your Name <mwaurarita2019@gmail.com>
2019-08-30 15:26:17 +0300

    function for save&& create

--- run.py
+++ run.py
@@ -3,3 +3,12 @@ from user import User
 from credential import Credential
 
 
+def create_user(first_name, last_name, phone, password):
+    
+    new_user = User(first_name, last_name, phone, password)
+    return new_user
+
+
+def save_users(user):
+
+    user.save_user()
