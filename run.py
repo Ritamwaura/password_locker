348b4862b1460af7f4d1a5fdb768bf2c9a64eba1
Your Name <mwaurarita2019@gmail.com>
2019-08-30 15:32:57 +0300

    generating passwords function and fixed indentation

--- run.py
+++ run.py
@@ -3,8 +3,9 @@ from user import User
 from credential import Credential
 
 
+
 def create_user(first_name, last_name, phone, password):
-    
+
     new_user = User(first_name, last_name, phone, password)
     return new_user
 
@@ -12,10 +13,10 @@ def create_user(first_name, last_name, phone, password):
 def save_users(user):
 
     user.save_user()
-    
-    
+
+
 def del_user(user):
-    
+
     user.delete_user()
 
 
@@ -26,3 +27,109 @@ def find_user(name):
 def check_existing_users(name):
 
     return User.user_exist(name)
+
+
+def display_users():
+
+    return User.display_users()
+
+def user_log_in(name, password):
+    '''
+    Function that allows a user to log into their credential account
+    Args:
+        name : the name the user used to create their user account
+        password : the password the user used to create their user account
+    '''
+    log_in = User.log_in(name, password)
+    if log_in != False:
+        return User.log_in(name, password)
+
+
+
+def main():
+    print("Welcome to your  password locker. What is your name?")
+    user_name = input()
+
+    print(f"Hi {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+                    print(
+                        "Use these cheat codes to navigate through : yo - create a new user, lo - display users, wo -find a user,lol - log users ,ex -exit the users list ")
+
+                    cheat_code = input().lower()
+
+                    if cheat_code == 'yo':
+                            print("New Credentials")
+                            print("-"*10)
+
+                            print("First name ....")
+                            f_name = input()
+
+                            print("Last name ...")
+                            l_name = input()
+
+                            print("Username ...")
+                            p_name = input()
+
+                            print("password ...")
+                            u_name = input()
+
+                            # create and save new contact.
+                            save_users(create_user(
+                                f_name, l_name, p_name, u_name))
+                            print('\n')
+                            print(f"New User {f_name} {l_name} created")
+                            print('\n')
+
+                    elif cheat_code == 'lo':
+
+                            if display_users():
+                                    print("Here is a list of all your users")
+                                    for user in display_users():
+                                            print(
+                                                f"{user.first_name} {user.last_name} .....{user.username}")
+                            else:
+                                    print("You dont seem to have any users saved yet")
+                                    
+                    elif cheat_code == 'lol':
+                                    print("Log into Password Locker Account")
+                                    print("Enter the user name")
+                                    username = input()
+                                    if user_log_in(username,u_name) == "":
+                                        print("Please try again or create an account")
+                                    else:
+                                        user_log_in(username,u_name)
+                                        print(f'''{username} welcome to your Credentials\n
+                                        Use these cheat codes to get around''')
+                                        print("Enter the password")
+                                        u_name = input()
+
+                    elif cheat_code == 'wo':
+
+                            print("Enter the username you want to search for")
+
+                            search_name = input()
+                            if check_existing_users(search_name):
+                                    search_user = find_user(search_name)
+                                    print(
+                                        f"{search_user.first_name} {search_user.last_name}")
+                                    print('-' * 20)
+
+                                    print(
+                                        f"Username.......{search_user.username}")
+                                    print(
+                                        f"Password.......{search_user.password}")
+                            else:
+                                    print("That user does not exist")
+
+                    elif cheat_code == "ex":
+                            print("It was nice for  you having to use my program feel free to use it again  ")
+                            break
+                    else:
+                            print(
+                                "I really didn't get that. Please use the cheat codes")
+
+if __name__ == '__main__':
+
+    main()
---
+++ test_credential.py
@@ -0,0 +1,40 @@
+import string
+import pyperclip
+from random import choice
+class Credential:
+
+    credential_list = []  
+
+
+def __init__(self, account, password, account_password):
+
+
+
+        self.account = account
+        self.password = account_password
+        self.account_password = account_password
+
+def save_credential(self):
+
+        '''
+        save_credential method saves credentials objects into credential_list
+        '''
+
+        Credential.credential_list.append(self) 
+        
+@classmethod
+def generate_password(cls):
+        '''
+        Method that generates a random alphanumeric password
+        '''
+        
+        size = 9
+
+
+
+        alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+
+        password = ''.join( choice(alphanum) for num in range(size) )
+        
+        return password
