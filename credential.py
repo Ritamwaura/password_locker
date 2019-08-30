e90b3e694d4afbf534edaa4b9d3ecf33f8385212
Your Name <mwaurarita2019@gmail.com>
2019-08-30 13:48:09 +0300

    create an instance  and passed in properties the object credential

--- credential.py
+++ credential.py
@@ -3,3 +3,11 @@
 
 class credential:
     credential_list = []
+    
+    
+def __init__(self,account, password,account_password):
+    
+    
+    self.account = account
+    self.password = account_password
+    self.account_password = account_password
